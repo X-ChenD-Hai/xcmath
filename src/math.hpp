93bd0d80f8d8712fa65ec1f925ac442dbcecf133
XCDH <illuminatestar@foxmail.com>
2025-01-19 16:37:48 +0800

    完善mobject,utils

---
+++ src/math.hpp
@@ -0,0 +1,9 @@
+// math.hpp
+#ifndef MATH_H
+#define MATH_H
+#include "./mobject/complex.hpp"     // IWYU pragma: export
+#include "./mobject/mat.hpp"         // IWYU pragma: export
+#include "./mobject/quaternion.hpp"  // IWYU pragma: export
+#include "./mobject/vec.hpp"         // IWYU pragma: export
+namespace math {}
+#endif  // MATH_H
--- test/test1.cc
+++ test/test1.cc
@@ -1,5 +1,23 @@
+#include <cmath>  // IWYU pragma: keep
+#include <cstddef>
 #include <iostream>
+
+#include "math.hpp"
+#include "mobject/complex.hpp"
+#include "mobject/vec.hpp"
+#include "utils/show.hpp"
 
 using namespace std;
 
-int main() { return 0; }
+int main() {
+    using namespace std;
+    using namespace math;
+    auto x = vec3<int>(1, 2, 3);
+    auto x2 = vec2<int>(1, 2);
+    auto x3 = vec3<int>(x2, 3);
+    mat<int, 3, 3> m(x, vec<int, 3>{1}, x3);
+    quaternion<int> q(1, 2, 3, 4);
+    cout << m << endl;
+    cout << x3 << endl;
+    return 0;
+}
