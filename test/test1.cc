7f3a304d5af971e0d6a59168119234ae1a6c051b
XCDH <illuminatestar@foxmail.com>
2025-01-19 16:42:35 +0800

    完善mat,vec,quaternion

--- test/test1.cc
+++ test/test1.cc
@@ -1,11 +1,8 @@
 #include <cmath>  // IWYU pragma: keep
-#include <cstddef>
 #include <iostream>
 
 #include "math.hpp"
-#include "mobject/complex.hpp"
 #include "mobject/vec.hpp"
-#include "utils/show.hpp"
 
 using namespace std;
 
@@ -15,9 +12,8 @@ int main() {
     auto x = vec3<int>(1, 2, 3);
     auto x2 = vec2<int>(1, 2);
     auto x3 = vec3<int>(x2, 3);
-    mat<int, 3, 3> m(x, vec<int, 3>{1}, x3);
     quaternion<int> q(1, 2, 3, 4);
-    cout << m << endl;
-    cout << x3 << endl;
+    auto m = mat<int, 3, 2>{vec2<int>(1, 2), vec2<int>{3, 4}, vec2<int>{5, 6}};
+
     return 0;
 }
