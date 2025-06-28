ba8caecc96beda0099de95c998093f93daa4b281
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-06-28 21:57:24 +0800

    feat(xcmath): 添加 tvector 模板结构
    引入一个新的 tvector 模板结构，用于处理类型向量的操作。tvector 支持添加、删除、查找和切片等操作。
    
    - 定义了 TVector 概念，用于检查类型是否符合 tvector 的要求。
    - 实现了 tvector 的基本结构，包括静态成员 size 和 has。
    - 实现了 tvector 的 push_back、push_front、pop_back、pop_front、concat、remove、remove_all、remove_all_from_list、find 和 subsequence 操作。
    - 添加了多种测试用例，验证 tvector 的功能是否正确，包括对空 tvector 和不同类型的 tvector 进行操作的验证。

--- tests/utils.cc
+++ tests/utils.cc
@@ -1,13 +1,34 @@
 #include <gtest/gtest.h>
 
-#include <iostream>
 #include <utils/compiling.hpp>
+#include <utils/types.hpp>
 
 using namespace std;
 using namespace xcmath;
 TEST(Compiling, get_type_name) {
-
     EXPECT_STREQ(TypeName<int>, "int");
     EXPECT_STREQ(TypeName<double>, "double");
     EXPECT_STREQ(TypeName<float>, "float");
+}
+
+TEST(Compiling, tvector) {
+    using integer = tvector<char, short, int, long, long long>;
+    using floating = tvector<long double, double, float>;
+    using number = integer::concat<floating>::push_back<bool>;
+    using int1 = integer::pop_back;
+    using int2 = integer::pop_front;
+    using int43 = integer::remove_all<>;
+    using int44 = integer::remove_all<int, short>;
+    using int45 = integer::remove_all_from_list<tvector<>>;
+    using int46 = integer::remove_all_from_list<tvector<int>>;
+    using int47 = integer::remove_all_from_list<tvector<int, long>>;
+    using t1 = tvector<int, double, float>::remove<0>;
+    using t2 = tvector<int, double, float>::slice<0, 2>;
+    EXPECT_FALSE(tvector<>::has<int>);
+    EXPECT_FALSE(tvector<>::has<double>);
+    EXPECT_EQ(tvector<int>::size, 1);
+    EXPECT_EQ((tvector<int, double>::size), 2);
+    EXPECT_EQ((tvector<int, double, float>::size), 3);
+    EXPECT_EQ(t1::size, 2);
+    EXPECT_EQ(t2::size, 2);
 }
